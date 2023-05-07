ff070c46b88c2f5942a5152c6d76b39e33cd2159
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-05-07 10:42:52 +0300

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Иван Вдовин on 07.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
